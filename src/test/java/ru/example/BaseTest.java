4bc9bf39af4c6623dc1600348b6caf380ceb2611
NeverFlyDog <217641660+NeverFlyDog@users.noreply.github.com>
2025-08-16 03:13:39 +0300

    Add system properties support

--- src/test/java/ru/example/BaseTest.java
+++ src/test/java/ru/example/BaseTest.java
@@ -6,29 +6,35 @@ import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import ru.example.utils.AllureAttachments;
+import ru.example.utils.Config;
 
 import java.util.Map;
-
-import static io.qameta.allure.Allure.step;
 
 public abstract class BaseTest {
 
     @BeforeAll
     static void configureSelenide() {
-        Configuration.browserSize = "1920x1080";
+        Configuration.browser = Config.BROWSER;
+        Configuration.browserVersion = Config.BROWSER_VERSION;
+        Configuration.browserSize = Config.BROWSER_SIZE;
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
 
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        Configuration.remote = Config.REMOTE_URL;
+
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.of(
                 "enableVNC", true,
                 "enableVideo", true
         ));
         Configuration.browserCapabilities = capabilities;
+    }
 
+    @BeforeEach
+    void addListener() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     }
 
